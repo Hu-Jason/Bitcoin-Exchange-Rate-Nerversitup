52932e3de2a5b72933bcae9bb7b8de46fad31144
Hu Jincheng <fujinceng@outlook.com>
2022-11-08 12:10:07 +0800

    Update Additional Algorithm Tests.swift
    reanalyzed time complexity of filtering prime number algorithm
    time complexity of the prime number function is O(n!)

--- Additional Algorithm Tests/Additional Algorithm Tests.swift
+++ Additional Algorithm Tests/Additional Algorithm Tests.swift
@@ -43,7 +43,7 @@ func primes(n: Int) -> [Int] {
 
 primes(n: 8) //will return [2, 3, 5, 7]
 primes(n: 20) //will return [2, 3, 5, 7, 11, 13, 17, 19]
-//Time complexity O(n^2)
+//Time complexity O(n!)
 //Space complexity O(n)
 
 
